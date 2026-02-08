dcd68de3cbc022110747923c7eb2c1fed3c0789b
Andrei Isayenka <aliceverssa@gmail.com>
2026-02-09 00:19:45 +0100

    Project setup

---
+++ ABCBankTestTask/ABCBankTestTask/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ABCBankTestTask
+//
+//  Created by Alice Versa on 09/02/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
